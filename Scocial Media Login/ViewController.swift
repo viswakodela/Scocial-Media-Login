0cd48b716d5be5a06c65f84bb672ebc682eccd00
viswa kodela <viswakodela@viswas-mbp.local>
2018-07-19 11:49:37 -0400

    Initial Commit

---
+++ Scocial Media Login/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scocial Media Login
+//
+//  Created by Viswa Kodela on 7/19/18.
+//  Copyright © 2018 Viswa Kodela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
